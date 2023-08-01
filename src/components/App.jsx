a26aeb985da5657dda73415fe9cf2bd881a417a8
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-08-01 22:51:51 +0300

    HMK-31/07/23

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import Triangle from './Loader/Loader';
 import receivingImages from './services/api';
 
 const IMAGES_PER_PAGE = 12;
-
+document.title = 'HMK-3 iMAGE'
 class App extends Component {
   state = {
     images: [],
